37ec21eb718ba870fb11811551845db198565db6
克喵爱吃卤面 <kemiaodwi@gmail.com>
2025-08-22 18:56:09 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -32,10 +32,10 @@ export default defineConfig({
   // Adapter
   // https://docs.astro.build/en/guides/deploy/
   // 1. Vercel (serverless)
-  adapter: vercel(),
-  output: 'server',
-  // 2. Vercel (static)
-  // adapter: vercelStatic(),
+ // adapter: vercel(),
+ // output: 'server',
+   2. Vercel (static)
+   adapter: vercelStatic(),
   // 3. Local (standalone)
   // adapter: node({ mode: 'standalone' }),
   // output: 'server',
